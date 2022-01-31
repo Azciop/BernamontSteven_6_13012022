d56516a22efdb3067cfb20f8823c0047fe2c0f84
Steven Bernamont <stevenbernamontcontact@gmail.com>
2022-01-31 11:38:06 +0100

    added get routes

--- app/controllers/sauce.js
+++ app/controllers/sauce.js
@@ -2,12 +2,12 @@ const Sauce = require("../models/sauce");
 
 exports.getOneSauce = (req, res, next) => {
   Sauce.findOne({ _id: req.params.id })
-  .then((sauce) => res.status(200).json(sauce))
-  .catch((error) => res.status(404).json({ error }));
+    .then((sauce) => res.status(200).json(sauce))
+    .catch((error) => res.status(404).json({ error }));
 };
 
 exports.getAllSauces = (req, res, next) => {
   Sauce.find()
-  .then((sauces) => res.status(200).json(sauces))
-  .catch((error) => res.status(400).json({ error }));
+    .then((sauces) => res.status(200).json(sauces))
+    .catch((error) => res.status(400).json({ error }));
 };
