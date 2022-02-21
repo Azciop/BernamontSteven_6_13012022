ebacc4fb05b5c79f645906be179ef86a6a587721
Steven Bernamont <stevenbernamontcontact@gmail.com>
2022-02-21 15:58:22 +0100

    added last rgpd function and comments

--- app/controllers/sauce.js
+++ app/controllers/sauce.js
@@ -1,187 +1,233 @@
 // getting the sauce model's file
 const Sauce = require("../models/sauce");
 
+// importing the filesystem module
 const fs = require("fs");
 
 // making a function and an export to find a specific sauce
 exports.readOneSauce = (req, res, next) => {
-  Sauce.findOne({ _id: req.params.id })
-    .then(sauce => {
-      sauce.imageUrl = `${req.protocol}://${req.get("host") + sauce.imageUrl}`;
-      res.status(200).json(sauce);
-    })
-    .catch(error => res.status(404).json({ error }));
+	// using the findOne function to find a sauce
+	Sauce.findOne({ _id: req.params.id })
+		.then(sauce => {
+			// then, pushing the sauce and it's image
+			sauce.imageUrl = `${req.protocol}://${req.get("host") + sauce.imageUrl}`;
+			res.status(200).json(sauce);
+		})
+		.catch(error => res.status(404).json({ error }));
 };
 
 // making a function and an export to read every sauces
 exports.readAllSauces = (req, res, next) => {
-  Sauce.find()
-    .then(sauces => {
-      sauces = sauces.map(sauce => {
-        sauce.imageUrl = `${req.protocol}://${req.get("host") + sauce.imageUrl
-          }`;
-        return sauce;
-      });
-      res.status(200).json(sauces);
-    })
-    .catch(error => res.status(400).json({ error }));
+	// using a find function to find every sauces available
+	Sauce.find()
+		.then(sauces => {
+			// using a .map to make an array that includes every sauces
+			sauces = sauces.map(sauce => {
+				// pushing every sauces and their image
+				sauce.imageUrl = `${req.protocol}://${
+					req.get("host") + sauce.imageUrl
+				}`;
+				return sauce;
+			});
+			res.status(200).json(sauces);
+		})
+		.catch(error => res.status(400).json({ error }));
 };
 
 // making a function using a switch to make the like system functionnal
 exports.rateSauce = (req, res, next) => {
-  Sauce.findOne({ _id: req.params.id }).then(sauce => {
-    switch (req.body.like) {
-      //   case one, we add a like by using a push method
-      case 1:
-        if (!sauce.usersLiked.includes(req.body.userId)) {
-          let toChange = {
-            $inc: { likes: +1 },
-            $push: { usersLiked: req.body.userId },
-          };
-          if (sauce.usersDisliked.includes(req.body.userId)) {
-            // if user wants to change his dislike to put a like, we push the like and we pull out the dislike
-            toChange = {
-              $inc: { likes: +1, dislikes: -1 },
-              $push: { usersLiked: req.body.userId },
-              $pull: { usersDisliked: req.body.userId },
-            };
-          }
-          Sauce.updateOne(
-            // we update the result for the like
-            { _id: req.params.id },
-            toChange
-          )
-            .then(sauce => res.status(200).json({ message: "Liked !" }))
-            .catch(error => res.status(400).json({ error }));
-        } else {
-          res.status(304).json();
-        }
-        break;
-      case -1:
-        // case 2, we add a dislike by using the push method
-        if (!sauce.usersDisliked.includes(req.body.userId)) {
-          let toChange = {
-            $inc: { dislikes: req.body.like++ * -1 },
-            $push: { usersDisliked: req.body.userId },
-          };
-          if (sauce.usersLiked.includes(req.body.userId)) {
-            // if the user wants to change his like to a dislike, we push the dislike and we pull out the like
-            toChange = {
-              $inc: { likes: -1, dislikes: +1 },
-              $push: { usersDisliked: req.body.userId },
-              $pull: { usersLiked: req.body.userId },
-            };
-          }
-          Sauce.updateOne(
-            // then we update the result
-            { _id: req.params.id },
-            toChange
-          )
-            .then(sauce => res.status(200).json({ message: "Disliked !" }))
-            .catch(error => res.status(400).json({ error }));
-        } else {
-          res.status(304).json();
-        }
-        break;
-      case 0:
-        Sauce.findOne({ _id: req.params.id })
-          .then(sauce => {
-            // case 2, we want to take off a like or a dislike
-            if (sauce.usersLiked.includes(req.body.userId)) {
-              Sauce.updateOne(
-                { _id: req.params.id },
-                //  we use a pull method to take off a like
-                { $pull: { usersLiked: req.body.userId }, $inc: { likes: -1 } }
-              )
-                .then(sauce => {
-                  res.status(200).json({ message: "Sauce unliked" });
-                })
-                .catch(error => res.status(400).json({ error }));
-            } else if (sauce.usersDisliked.includes(req.body.userId)) {
-              Sauce.updateOne(
-                { _id: req.params.id },
-                {
-                  //  we use a pull method to take off a dislike
-                  $pull: { usersDisliked: req.body.userId },
-                  $inc: { dislikes: -1 },
-                }
-              )
-                .then(sauce => {
-                  res.status(200).json({ message: "Sauce undisliked !" });
-                })
-                .catch(error => res.status(400).json({ error }));
-            } else {
-              res.status(304).json();
-            }
-          })
-          .catch(error => res.status(400).json({ error }));
+	// using findOne function to find the sauce
+	Sauce.findOne({ _id: req.params.id }).then(sauce => {
+		switch (req.body.like) {
+			//   case one, we add a like by using a push method
+			case 1:
+				// Making a if to check if user didn't liked the sauce yet
+				if (!sauce.usersLiked.includes(req.body.userId)) {
+					// making a object with $inc and $push methods to add a like and to add the user's id
+					let toChange = {
+						$inc: { likes: +1 },
+						$push: { usersLiked: req.body.userId },
+					};
+					if (sauce.usersDisliked.includes(req.body.userId)) {
+						// if user wants to change his dislike to put a like, we push the like and we pull out the dislike
+						toChange = {
+							$inc: { likes: +1, dislikes: -1 },
+							$push: { usersLiked: req.body.userId },
+							$pull: { usersDisliked: req.body.userId },
+						};
+					}
+					// we update the result for the like
+					Sauce.updateOne({ _id: req.params.id }, toChange)
+						// then we send the result and the message
+						.then(sauce => res.status(200).json({ message: "Liked !" }))
+						.catch(error => res.status(400).json({ error }));
+				} else {
+					res.status(304).json();
+				}
+				break;
+			case -1:
+				// case 2, we add a dislike by using the push method
+				// we check if the user has already disliked the sauce
+				if (!sauce.usersDisliked.includes(req.body.userId)) {
+					// making an object with $inc and $push methods to add the dislike and the user's id
+					let toChange = {
+						$inc: { dislikes: req.body.like++ * -1 },
+						$push: { usersDisliked: req.body.userId },
+					};
+					if (sauce.usersLiked.includes(req.body.userId)) {
+						// if the user wants to change his like to a dislike, we push the dislike and we pull out the like
+						toChange = {
+							$inc: { likes: -1, dislikes: +1 },
+							$push: { usersDisliked: req.body.userId },
+							$pull: { usersLiked: req.body.userId },
+						};
+					}
+					// then we update the result
+					Sauce.updateOne({ _id: req.params.id }, toChange)
+						.then(sauce => res.status(200).json({ message: "Disliked !" }))
+						.catch(error => res.status(400).json({ error }));
+				} else {
+					res.status(304).json();
+				}
+				break;
+			case 0:
+				Sauce.findOne({ _id: req.params.id })
+					.then(sauce => {
+						// case 3, we want to take off a like or a dislike
+						if (sauce.usersLiked.includes(req.body.userId)) {
+							// using the updateOne function to update the result
+							Sauce.updateOne(
+								{ _id: req.params.id },
+								//  we use a pull method to take off a like
+								{ $pull: { usersLiked: req.body.userId }, $inc: { likes: -1 } }
+							)
+								.then(sauce => {
+									// then we send the result and the message
+									res.status(200).json({ message: "Sauce unliked" });
+								})
+								.catch(error => res.status(400).json({ error }));
+							// if it's a dislike, we take it off too
+						} else if (sauce.usersDisliked.includes(req.body.userId)) {
+							Sauce.updateOne(
+								{ _id: req.params.id },
+								{
+									//  we use a pull method to take off a dislike
+									$pull: { usersDisliked: req.body.userId },
+									$inc: { dislikes: -1 },
+								}
+							)
+								.then(sauce => {
+									// then we push the result
+									res.status(200).json({ message: "Sauce undisliked !" });
+								})
+								.catch(error => res.status(400).json({ error }));
+						} else {
+							res.status(304).json();
+						}
+					})
+					.catch(error => res.status(400).json({ error }));
 
-        break;
-    }
-  });
+				break;
+		}
+	});
 };
 
 // we make a function and a exports to creat a new sauce
 exports.createSauce = (req, res, next) => {
-  const sauceObject = JSON.parse(req.body.sauce);
-  delete sauceObject._id;
-  const sauce = new Sauce({
-    ...sauceObject,
-    imageUrl: `/images/${req.file.filename}`,
-  });
-  sauce
-    .save()
-    .then(() => res.status(201).json({ message: "New sauce saved !" }))
-    .catch(error => res.status(400).json({ error }));
+	// making a const to store new sauce values
+	const sauceObject = JSON.parse(req.body.sauce);
+	delete sauceObject._id;
+	// then we creat the new sauce using the cost we just created
+	const sauce = new Sauce({
+		...sauceObject,
+		// we also get the desired image
+		imageUrl: `/images/${req.file.filename}`,
+	});
+	sauce
+		.save()
+		// then we push the new sauce and we send a message
+		.then(() => res.status(201).json({ message: "New sauce saved !" }))
+		.catch(error => res.status(400).json({ error }));
 };
 
 // we make a function to update a sauce
 exports.updateSauce = (req, res, next) => {
-  Sauce.findOne({ _id: req.params.id })
-    .then(sauce => {
-      const filename = sauce.imageUrl.split("/images/")[1];
-      if (req.file) {
-        const sauceObject = {
-          ...JSON.parse(req.body.sauce),
-          imageUrl: `/images/${req.file.filename}`,
-        };
-        // We use the unlink method to delete the old picture
-        fs.unlink(`images/${filename}`, () => {
-          Sauce.updateOne(
-            { _id: req.params.id },
-            { ...sauceObject, _id: req.params.id }
-          )
-            .then(() =>
-              res.status(200).json({ message: "Your sauce has been modified" })
-            )
-            .catch(error => res.status(400).json({ error }));
-        });
-      } else {
-        Sauce.updateOne(
-          { _id: req.params.id },
-          { ...req.body, _id: req.params.id }
-        )
-          .then(() =>
-            res.status(200).json({ message: "Your sauce has been modified" })
-          )
-          .catch(error => res.status(400).json({ error }));
-      }
-    })
-    .catch(error => res.status(500).json({ error }));
+	// Using the findOne method to find the sauce
+	Sauce.findOne({ _id: req.params.id })
+		.then(sauce => {
+			// we make a const to find the image we want to delete in case of a image change
+			const filename = sauce.imageUrl.split("/images/")[1];
+			if (req.file) {
+				// we make a object that contains the new values and the new image
+				const sauceObject = {
+					...JSON.parse(req.body.sauce),
+					imageUrl: `/images/${req.file.filename}`,
+				};
+				// We use the unlink method to delete the old image
+				fs.unlink(`images/${filename}`, () => {
+					// using the updateOne function to update the values if the image has been modified
+					Sauce.updateOne(
+						{ _id: req.params.id },
+						{ ...sauceObject, _id: req.params.id }
+					)
+						// then we send the message
+						.then(() =>
+							res.status(200).json({ message: "Your sauce has been modified" })
+						)
+						.catch(error => res.status(400).json({ error }));
+				});
+			} else {
+				// else, we just update the new values without changing the image
+				Sauce.updateOne(
+					{ _id: req.params.id },
+					{ ...req.body, _id: req.params.id }
+				)
+					// then, we send the message
+					.then(() =>
+						res.status(200).json({ message: "Your sauce has been modified" })
+					)
+					.catch(error => res.status(400).json({ error }));
+			}
+		})
+		.catch(error => res.status(500).json({ error }));
 };
 
 // we make a function to delete a sauce
-
 exports.deleteSauce = (req, res, next) => {
-  Sauce.findOneAndDelete({ _id: req.params.id }).then(sauce => {
-    const filename = sauce.imageUrl.split("/images/")[1];
-    fs.unlink(`images/${filename}`, () => {
-      res.status(200).json({ message: "Your sauce has been deleted" });
-    }).catch(error => res.status(400).json({ error }));
-  });
+	// uding a findOneAndDelete to delete a sauce
+	Sauce.findOneAndDelete({ _id: req.params.id }).then(sauce => {
+		// making a const to get the image we want to delete
+		const filename = sauce.imageUrl.split("/images/")[1];
+		// using the unlink method to delete the image that is in the const
+		fs.unlink(`images/${filename}`, () => {
+			// then we send the image
+			res.status(200).json({ message: "Your sauce has been deleted" });
+		}).catch(error => res.status(400).json({ error }));
+	});
 };
+
+//
+// RGPD
+//
 
 // report a sauce
 exports.reportSauce = (req, res, next) => {
-  
-}
+	// using the findOneAndUpdate function to update the result
+	Sauce.findOneAndUpdate(
+		{ _id: req.params.id },
+		{
+			// using the $inc aand $push method to add a report and the user id
+			$inc: { report: 1 },
+			$push: { whoReported: req.auth.userId },
+		}
+	)
+		.then(sauce => {
+			// if sauce cant be found, send error message
+			if (!sauce) {
+				return res.status(404).json({ error: "Sauce not found !" });
+			}
+			// else, report the sauce and send a message
+			res.status(200).json({ message: "Sauce reported" });
+		})
+		.catch(error => res.status(400).json({ error }));
+};
--- app/controllers/user.js
+++ app/controllers/user.js
@@ -3,7 +3,12 @@ const User = require("../models/user");
 const jwt = require("jsonwebtoken");
 const user = require("../models/user");
 const Sauce = require("../models/sauce");
+
+// Importing the filesystem module
+
 const fs = require("fs");
+
+// making the sign up function
 exports.signup = (req, res, next) => {
 	bcrypt
 		.hash(req.body.password, 10)
@@ -20,19 +25,24 @@ exports.signup = (req, res, next) => {
 		.catch(error => res.status(500).json({ error }));
 };
 
+// making the log in function
 exports.login = (req, res, next) => {
+	// using findOne to find the user
 	User.findOne({ email: req.body.email })
 		.then(user => {
 			if (!user) {
 				return res.status(401).json({ error: "Utilisateur non trouvé !" });
 			}
 			bcrypt
+
+				// checking if the two passwords matches. If not, send error message
 				.compare(req.body.password, user.password)
 				.then(valid => {
 					if (!valid) {
 						return res.status(401).json({ error: "Mot de passe incorrect !" });
 					}
 					res.status(200).json({
+						// if passwords matches, creat random secret token for a duration of 24h, and log in
 						userId: user._id,
 						token: jwt.sign({ userId: user._id }, "RANDOM_TOKEN_SECRET", {
 							expiresIn: "24h",
@@ -44,14 +54,21 @@ exports.login = (req, res, next) => {
 		.catch(error => res.status(500).json({ error }));
 };
 
+//
+// RGPD
+//
+
 // find user and send infos in JSON
 exports.readUser = (req, res, next) => {
-	user
-		.findOne({ userId: req.auth.userId })
+	// using FindONe to find the user
+	User.findOne({ userId: req.auth.userId })
 		.then(user => {
+			// if user does not match, send error message
 			if (!user) {
 				return res.status(404).json({ error: "User not found!" });
 			}
+			// send user infos as json
+			res.status(200).json(user);
 		})
 		.catch(error => {
 			res.status(404).send({ error });
@@ -60,114 +77,84 @@ exports.readUser = (req, res, next) => {
 
 // find user and send infos in txt
 exports.exportUser = (req, res, next) => {
-	const userSauces = [];
-	User.findOne({ userId: req.auth.userId })
-		.then(user => {
-			if (!user) {
-				return res.status(404).json({ error: "User not found !" });
-			}
-			try {
-				const sauces = Sauce.find({ userId: user._id });
-				if (sauces.length <= 0) {
-					userSauces.push("No sayces were found !");
-				} else {
-					userSauces.push(sauces);
-				}
-			} catch (error) {
-				console.log(error);
-			}
+	// Using findOne to find the user
+	User.findOne({ userId: req.auth.userId }).then(user => {
+		// if user does not match, send error message
+		if (!user) {
+			return res.status(404).json({ error: "User not found !" });
+		}
+		// stringify user infos as txt file
+		const string = user.toString();
 
-			const result = { ...user._doc, sauces: userSauces };
-
-			const StringifyResults = JSON.stringify(result, null, 2);
-
-			fs.writeFile(__dirname + "/test.txt", StringifyResults, err => {
-				if (err) {
-					console.log("couldn't write any files", err);
-				} else {
-					console.log("Successfully wrote file");
-				}
-			});
-			return res.status(200).json(result);
-		})
-		.catch(error => res.status(500).json({ error }));
+		res.attachment("data.txt");
+		res.type("txt");
+		// send text file to user
+		return res.status(200).send(string);
+	});
 };
 
 // update user infos
-exports.updateUser = (req, res, next) => {
-	const filter = { userId: req.auth.userId };
-
+exports.updateUser = async (req, res, next) => {
+	// making an empty object to store future infos
 	const update = {};
-
+	// making the password change
 	if (req.body.password) {
-		const hash = bcrypt.hash(req.body.password, 10);
-
+		// Hashing the password
+		const hash = await bcrypt.hash(req.body.password, 10);
+		//changing the password
 		update.password = hash;
 	}
-
+	// Making the email change
 	if (req.body.email) {
-		if (!validateEmail(req.body.email)) {
-			return res.status(400).json({ error: "The specified email is invalid." });
-		}
+		// changing the  email
+		update.email = req.body.email;
 	}
-
-	User.findOneAndUpdate(filter, update, {
-		returnOriginal: true,
-		updatedExisting: true,
-	})
+	// using the findOneAndUpdate function to update the desired change
+	User.findOneAndUpdate({ userId: req.auth.userId }, update)
 		.then(user => {
-			res.status(201).json({ error });
+			// if user not found, send error message
+			if (!user) {
+				return res.status(404).json({ error: "User not found !" });
+			}
+			// else, sending the confirmation message and update the user's infos
+			res.status(201).json({ message: "user updated" });
 		})
 		.catch(error => res.status(400).json({ error }));
 };
 
 // delete the user
 exports.deleteUser = (req, res, next) => {
+	// using the findOneAndDelete function to delete the desired user
 	User.findOneAndDelete({ userId: req.auth.userId })
 		.then(user => {
+			// if user not found, send error message
 			if (!user) {
-				return res.status(404).send({
-					message: "User not found",
-				});
+				return res.status(404).json({ error: "User not found !" });
 			}
-			res.send({ message: "This user has been delete !" });
+			// else, delete the user
+			res.status(204).send();
 		})
-		.catch(err => {
-			return res.status(500).send({
-				message: "Error, user cant be deleted",
-			});
-		});
+		.catch(error => res.status(400).json({ error }));
 };
 
 // report the user
 exports.reportUser = (req, res, next) => {
-	const currentUser = req.auth.userID;
-	User.findOneAndUpdate(req.userId, {
-		new: true,
-		returnOriginal: true,
-		updatedExisting: true,
-	}).then(user => {
-		if (!user) {
-			return res.status(404).json({ error: new Error(" User not found ! ") });
+	// using the findOneAndUpdate function to add a report on a user
+	User.findOneAndUpdate(
+		{ _id: req.params.id },
+		{
+			// using the $inc method to add a report
+			$inc: { report: 1 },
+			// using the $push method to send the user id of the user who reported
+			$push: { whoReported: req.auth.userId },
 		}
-
-		if (user.usersAlert.indexOf(currentUser)) {
-			user.usersAlert.push(currentUser);
-			user.reports++;
-		}
-		if (!user.usersAlert.indexOf(currentUser)) {
-			return res.send(" It is not possible to report yourself ");
-		}
-
-		User.updateOne({ userID: req.userID }, user)
-			.then(() => {
-				res.status(200).json({
-					message:
-						" Report sent ! ",
-				});
-			})
-			.catch(err => {
-				res.status(500).json({ err });
-			});
-	});
+	)
+		.then(user => {
+			if (!user) {
+				// if user not found, send error message
+				return res.status(404).json({ error: "User not found !" });
+			} // else, report the user and send message
+			res.status(200).json({ message: "user reported" });
+		})
+		.catch(error => res.status(400).json({ error }));
 };
